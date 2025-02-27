a97dd6b62668b0e8844b0e71bcc6596ce156d533
Adzi Bilal <a.bilal@acclime.com>
2025-02-27 16:06:33 +0700

    fix: update webpack config and remove json-loader

--- next.config.js
+++ next.config.js
@@ -3,18 +3,16 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.json$/,
-      type: 'javascript/auto',
-      resolve: {
-        fullySpecified: false
-      }
-    });
-
-    // Menambahkan konfigurasi untuk package.json files
-    config.module.rules.push({
-      test: /package\.json$/,
-      loader: 'json-loader',
       type: 'javascript/auto'
     });
+
+    // Konfigurasi node polyfills
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false
+    };
 
     return config;
   }
